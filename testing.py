6059572ebb2c69f56a67948e2e1a498854e44c6a
katemikels <116658164+katemikels@users.noreply.github.com>
2024-04-13 11:37:56 -0400

    Add files via upload
    These are the files for the random moves working with the API. It quits when a win or loss is detected and prints the output to the terminal.

---
+++ testing.py
@@ -0,0 +1,15 @@
+import main.py
+
+import os
+
+import errno
+import pygame
+from appdirs import user_data_dir
+
+from .game import Game2048
+from .manager import GameManager
+
+import pyautogui
+
+run_game()
+
